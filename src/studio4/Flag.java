26aebee56ce80cfe45589d3923a29c2e014ca1e8
tietchandang <tietchandang@localhost>
2023-10-05 12:30:10 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,11 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.5,0.5, 0.32, 0.22);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5,0.5, 0.3, 0.2);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.5,0.5, 0.1);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,73 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape= in.next();
+		int red= in.nextInt();
+		int green= in.nextInt();
+		int blue= in.nextInt();
+		boolean filled= in.nextBoolean();
+		double p1= in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3= in.nextDouble();
+		double p4= in.nextDouble();
+		
+	if (shape.equals("rectangle"))
+	{
+		if (filled== true)
+			{
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.filledRectangle(p1, p2, p3, p4);
+			}
+			
+			
+		else
+		{
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.rectangle(p1, p2, p3, p4);
+			
+		}
+	}
+	else if (shape.equals("ellipse"))
+	{
+		if (filled== true)
+		{
+		StdDraw.setPenColor(red, green, blue);
+		StdDraw.filledEllipse(p1, p2, p3, p4);
+		}
+		
+		
+	else
+	{
+		StdDraw.setPenColor(red, green, blue);
+		StdDraw.ellipse(p1, p2, p3, p4);
 		
 	}
-}
+	}
+	else
+	{
+		if (filled== true)
+		{
+		double p5= in.nextDouble();
+		double p6=in.nextDouble();
+		StdDraw.setPenColor(red, green, blue);
+		double [] x = {p1, p3, p5};
+		double [] y = {p2, p4, p6};
+		StdDraw.filledPolygon(x, y);
+		
+		}
+		
+		
+	else
+	{
+		double p5= in.nextDouble();
+		double p6=in.nextDouble();
+		StdDraw.setPenColor(red, green, blue);
+		StdDraw.line(p1, p2, p3, p4);
+		StdDraw.line(p3, p4, p5, p6);
+		StdDraw.line(p5, p6, p1, p2);
+	}
+	}
+	}
+	}
+	
+
